381d141e6412754c92c52188b99a288e468d2d42
MessirVoland <messir-voland@mail.ru>
2019-07-01 18:38:39 +0300

    Initial commit

---
+++ desktop/src/com/detone_studio/platform/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.detone_studio.platform.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.detone_studio.platform.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
