2e59c2b5eefeae67ac348c8bd1aebbe43d66486a
MessirVoland <30870916+messirvoland@users.noreply.github.com>
2020-02-03 16:51:48 +0300

    Пересобрал под новый грэдл

--- core/src/com/detone_studio/platform/Main.java
+++ core/src/com/detone_studio/platform/Main.java
@@ -1,11 +1,9 @@
 package com.detone_studio.platform;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.detone_studio.platform.States.TestState;
 
