56bcce654806e56689203e5fc3f9a5fb95c397d9
MessirVoland <messir-voland@mail.ru>
2020-03-03 17:11:28 +0300

    Clickes for cocos2d

--- core/src/com/detone_studio/platform/States/CocosTestState.java
+++ core/src/com/detone_studio/platform/States/CocosTestState.java
@@ -2,12 +2,14 @@ package com.detone_studio.platform.States;
 
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.detone_studio.platform.GameStateManager;
@@ -24,22 +26,27 @@ public class CocosTestState extends State {
         stage = new Stage(new ScreenViewport(new OrthographicCamera(480,600)));
         character_hero2 = new Character_hero2();
 
+        character_hero2.setBounds(0,0,250,250);
+
+        character_hero2.setTouchable(Touchable.enabled);
         character_hero2.addListener(new InputListener(){
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
                 System.out.println("CLICKED");
-                return false;
+                return true;
             }
 
             public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
                 System.out.println("REMOVED");
             }
         });
+
         MoveToAction action = new MoveToAction();
         //action.setPosition(100,100);
         //action.setDuration(5);
         //character_hero2.addAction(action);
         stage.addActor(character_hero2);
 
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
